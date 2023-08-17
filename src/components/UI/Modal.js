c2de136606b02127f780bf68da3ff1ce5480a3bb
Ben Daniels <benjamindaniels21@gmail.com>
2023-08-17 19:16:58 -0400

    bug fix in modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -19,11 +19,11 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {(ReactDOM.createPortal(<Backdrop />), portalElement)}
-      {
-        (ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>),
-        portalElement)
-      }
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
     </Fragment>
   );
 };
