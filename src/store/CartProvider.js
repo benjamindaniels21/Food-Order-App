072dbbff483a069f3db29cf4f94409ee827cee84
Ben Daniels <benjamindaniels21@gmail.com>
2023-08-19 21:41:25 -0400

    configure CartProvider wrapper and update App

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,7 +2,20 @@ import React from "react";
 import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
-  return <div>CartProvider</div>;
+  const addItemToCartHandler = (item) => {};
+  const removeItemFromCartHandler = (item) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
